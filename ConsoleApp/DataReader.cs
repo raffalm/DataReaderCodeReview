db3d6b824544f6b2872380dd10735f92951a3bd3
Rafal <rafalmika133@gmail.com>
2022-11-08 13:57:58 +0100

    fix: add getters and setters to all public properties

--- ConsoleApp/DataReader.cs
+++ ConsoleApp/DataReader.cs
@@ -112,19 +112,16 @@
     internal class ImportedObject : ImportedObjectBaseClass
     {
 
-        public string Schema;
+        public string Schema { get; set; }
 
-        public string ParentName;
+        public string ParentName { get; set; }
 
-        public string ParentType
-        {
-            get; set;
-        }
+        public string ParentType { get; set; }
 
         public string DataType { get; set; }
         public string IsNullable { get; set; }
 
-        public int NumberOfChildren;
+        public int NumberOfChildren { get; set; }
     }
 
     internal class ImportedObjectBaseClass
