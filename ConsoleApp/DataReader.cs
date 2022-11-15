308ce9dea82152196fa12097235654ed689b4694
Rafal <rafalmika133@gmail.com>
2022-11-15 11:20:31 +0100

    refactor:  change implementation data print

--- ConsoleApp/DataReader.cs
+++ ConsoleApp/DataReader.cs
@@ -63,7 +63,7 @@
                 importedObject.Name = importedObject.Name.Trim().Replace(" ", "");
                 importedObject.Schema = importedObject.Schema.Trim().Replace(" ", "");
                 importedObject.ParentName = importedObject.ParentName.Trim().Replace(" ", "");
-                importedObject.ParentType = importedObject.ParentType.Trim().Replace(" ", "");
+                importedObject.ParentType = importedObject.ParentType.Trim().Replace(" ", "").ToUpper();
             }
 
             // assign number of children
@@ -72,40 +72,24 @@
                 parent.NumberOfChildren = ImportedObjects
                     .Count(x => x.ParentType == parent.Type && x.ParentName == parent.Name);
             }
-            
-            foreach (var database in ImportedObjects)
+
+            var databases = ImportedObjects.Where(x => x.Type == "DATABASE");
+            foreach (var database in databases)
             {
-                if (database.Type == "DATABASE")
+                Console.WriteLine($"Database '{database.Name}' ({database.NumberOfChildren} tables)");
+                var tables = ImportedObjects.Where(x => x.ParentType == database.Type && x.ParentName == database.Name);
+                foreach (var table in tables)
                 {
-                    Console.WriteLine($"Database '{database.Name}' ({database.NumberOfChildren} tables)");
-
-                    // print all database's tables
-                    foreach (var table in ImportedObjects)
+                    Console.WriteLine($"\tTable '{table.Schema}.{table.Name}' ({table.NumberOfChildren} columns)");
+                    var columns = ImportedObjects.Where(x => x.ParentType == table.Type && x.ParentName == table.Name);
+                    foreach (var column in columns)
                     {
-                        if (table.ParentType.ToUpper() == database.Type)
-                        {
-                            if (table.ParentName == database.Name)
-                            {
-                                Console.WriteLine($"\tTable '{table.Schema}.{table.Name}' ({table.NumberOfChildren} columns)");
-
-                                // print all table's columns
-                                foreach (var column in ImportedObjects)
-                                {
-                                    if (column.ParentType.ToUpper() == table.Type)
-                                    {
-                                        if (column.ParentName == table.Name)
-                                        {
-                                            Console.WriteLine($"\t\tColumn '{column.Name}' with {column.DataType} data type {(column.IsNullable == "1" ? "accepts nulls" : "with no nulls")}");
-                                        }
-                                    }
-                                }
-                            }
-                        }
+                        Console.WriteLine($"\t\tColumn '{column.Name}' with {column.DataType} data type {(column.IsNullable == "1" ? "accepts nulls" : "with no nulls")}");
                     }
                 }
-            }
 
-            Console.ReadLine();
+            }
+                Console.ReadLine();
         }
     }
 
